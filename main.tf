624c48c56287cfc1312cf61905413a305dbece38
Amaranth T V <amarkuttan17@gmail.com>
2026-04-23 09:04:20 +0530

    Change AMI to Ubuntu for EC2 instance

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "public_subnet" {
 }
 
 resource "aws_instance" "ec2" {
-  ami           = "ami-0aa31b568c1e8d622" # Amazon Linux 2023 (x86_64) in ap-south-2
+  ami           = "ami-070e5bd3ff10324f8" # Ubuntu
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.public_subnet.id
   key_name      = "MCA-EC2-Key"           # Your key pair for SSH access
